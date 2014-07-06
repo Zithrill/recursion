9cade0bc7e0cdf8442c2c3293bdb887bc0d8f089
Jacob Gribschaw <jacob.gribschaw@gmail.com>
2014-07-06 12:52:15 -0700

    Cleaned up the code a bit more

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,14 +4,14 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className){
-    var classElements = new Array();
-    var allNodesOnDocument = document.all;
-    var pattern = new RegExp("(^|\\s)"+className+"(\\s|$)");
-    for (var i = 0; i < allNodesOnDocument.length; i++) {
-        if(pattern.test(allNodesOnDocument[i].className)){
-            classElements.push(allNodesOnDocument[i]);
-        }
+var getElementsByClassName = function (className) {
+  var classElements = [];
+  var allNodesOnDocument = document.all;
+  var pattern = new RegExp("(^|\\s)" + className + "(\\s|$)");
+  for (var i = 0; i < allNodesOnDocument.length; i++) {
+    if (pattern.test(allNodesOnDocument[i].className)) {
+      classElements.push(allNodesOnDocument[i]);
     }
-    return classElements;
+  }
+  return classElements;
 };
