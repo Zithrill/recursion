2c9f6a7c4fa30651aa42d03b35e019f479b4f3d0
Jacob Gribschaw <jacob.gribschaw@gmail.com>
2014-07-06 17:29:25 -0700

    Massive clean up and reworking to better use recursive calls and returns with less overhead

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -2,48 +2,24 @@
 // var stringifyJSON = JSON.stringify;
 
 // but you don't so you're going to write it from scratch:
-// I feel like this function is overly bulky...
-var stringifyJSON = function(obj) {
-var stringifyed = "";
-var numberOfIterations = 0;
-var lastChar;
-var stringify = function(argument) {
-	numberOfIterations++;
-	lastChar = stringifyed.charAt(stringifyed.length -1)
-	if (numberOfIterations > 2 && lastChar !== "[" && lastChar !== ":") {
-		stringifyed += ",";
-	}
-	if (typeof(argument) === "number" || argument) {
-		if (typeof(argument) === "string") {
-			var tempString = '"' + argument + '"';
-			tempString = '"' + argument + '"';
-			stringifyed += tempString;
-		}else if (Array.isArray(argument)) {
-			stringifyed += "[";
-			for (var i = 0; i < argument.length; i++) {
-				stringify(argument[i]);
-			}
-			stringifyed += "]";
-		}else if (typeof(argument) === "object"){
-			stringifyed += "{"
-			for(entry in argument){
-				if (typeof(argument[entry]) !== "function" && typeof(argument[entry]) !== "undefined") {
-					stringifyed += '"' + entry + '"' + ":";
-					stringify(argument[entry]);
-					stringifyed += ","
-				}
-			}
-			stringifyed += "}";
-			stringifyed = stringifyed.replace(/(,}$)/, "}");
-		}else {
-			stringifyed += argument.toString();
-		}
-	}else if (argument === null) {
-		stringifyed += 'null';
-	}else if (argument === false) {
-		stringifyed += 'false';
-	}
-}
-stringify(obj);
-return stringifyed;
+
+var stringifyJSON = function (obj) {
+  if (obj === null) {
+    return 'null';
+  }else if (obj === false) {
+    return 'false';
+  }else if (typeof obj === 'number' || typeof obj === 'boolean') {
+    return obj.toString();
+  }else if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  }else if (Array.isArray(obj)) {
+    return '[' + obj.map(function (element) { return stringifyJSON(element); }).join(',') + ']';
+  }else if (typeof obj === 'object') {
+    var temp = "{" + Object.keys(obj).map(function (key) {
+        if (typeof obj[key] !== 'function' && obj[key] !== undefined) {
+          return stringifyJSON(key) + ':' + stringifyJSON(obj[key]);
+        }
+      }).filter(function (n) { return n !== undefined; }).join(',') + "}";
+    return temp;
+  }
 };
